1feaef94e40b3bd02fda5b9fac6439b213e087a8
Ankur Shrivastava <ashrivastava@zynga.com>
2013-04-13 00:46:59 +0530

    movies by genre

--- inc/sidebar.php
+++ inc/sidebar.php
@@ -5,6 +5,7 @@
 			<li><a href="/" >Home</a></li>
             <li><a href='movieall.php?char=0' >All Movies</a></li>
             <li><a href='movierecent.php' >Recently Added Movies</a></li>
+            <li><a href='moviegenre.php' >Movies By Genre</a></li>
 			<li><a href='tvshowall.php' >TV Shows</a></li>
             <li><a href='../'>Browse Folders</a></li>
             <li><a href='updates.php'>SatyaNAS Updates</a></li>
